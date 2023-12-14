98517e45f215f250296cc2146bea5941fe216a58
cacobaco <lcjmonizf09@gmail.com>
2023-12-14 15:11:50 +0000

    updated pass model

--- models/pass.js
+++ models/pass.js
@@ -14,11 +14,6 @@ const passSchema = new Schema(
             required: true,
             unique: true,
         },
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
         price: {
             type: Number,
             required: true,
