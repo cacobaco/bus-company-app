02e8fcc57d8580dfd00aee92ed706cf09a2df272
cacobaco <lcjmonizf09@gmail.com>
2023-12-14 14:26:36 +0000

    added pass controller

---
+++ controllers/passController.js
@@ -0,0 +1,87 @@
+import Pass from "../models/pass.js";
+
+// GET /passes
+export const getPasses = (req, res) => {
+    Pass.find({}, (err, passes) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Error getting passes.",
+                error: err,
+            });
+        }
+
+        return res.json(passes);
+    });
+};
+
+// GET /passes/:id
+export const getPass = (req, res) => {
+    Pass.findById(req.params.id, (err, pass) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Error getting pass.",
+                error: err,
+            });
+        }
+
+        if (!pass) {
+            return res.status(404).json({
+                message: "Pass not found",
+            });
+        }
+
+        return res.json(pass);
+    });
+};
+
+// POST /passes
+export const createPass = (req, res) => {
+    const pass = new Pass({
+        code: req.body.code,
+        user: req.body.user,
+        price: req.body.price,
+    });
+
+    pass.save((err, pass) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Error saving pass",
+                error: err,
+            });
+        }
+
+        return res.json({
+            message: "Pass saved successfully",
+            pass,
+        });
+    });
+};
+
+// PATCH /passes/:id
+export const updatePass = (req, res) => {
+    res.json({
+        message: "To be implemented",
+    });
+};
+
+// DELETE /passes/:id
+export const deletePass = (req, res) => {
+    Pass.findByIdAndDelete(req.params.id, (err, pass) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Error deleting pass",
+                error: err,
+            });
+        }
+
+        if (!pass) {
+            return res.status(404).json({
+                message: "Pass not found",
+            });
+        }
+
+        return res.json({
+            message: "Pass deleted successfully",
+        });
+    });
+};
